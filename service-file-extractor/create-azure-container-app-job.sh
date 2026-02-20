9c588e92ae69518b94950609a9de3163b8ce30ca
Genry Leyva <gleyvaglez@gmail.com>
2026-02-20 19:57:51 -0300

    fix: use explicit REGISTRY_NAME for ACR credential retrieval in job creation script and remove trailing newlines from Dockerfile.

--- service-file-extractor/create-azure-container-app-job.sh
+++ service-file-extractor/create-azure-container-app-job.sh
@@ -7,6 +7,7 @@ ACA_ENV="env-licitaciones"
 ACR_USER_NAME="accsalicitaciones"
 SUBSCRIPTION_ID="d3fbaef6-2413-47bf-be3d-2019470dc20e"
 REGISTRY="accsalicitaciones.azurecr.io"
+REGISTRY_NAME="accsalicitaciones"
 
 APP_PATH="services"
 APP_NAME="service-file-extractor"
@@ -15,7 +16,7 @@ APP_TAG="latest"
 IMAGE="$REGISTRY/$APP_PATH/$APP_NAME:$APP_TAG"
 
 # Get ACR password
-ACR_PASSWORD=$(az acr credential show --name "$ACR_NAME" --query passwords[0].value -o tsv)
+ACR_PASSWORD=$(az acr credential show --name "$REGISTRY_NAME" --query passwords[0].value -o tsv)
 
 echo "Creating Container App Job '$APP_NAME'..."
 az containerapp job create \
